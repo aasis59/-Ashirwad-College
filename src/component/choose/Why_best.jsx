bfb0c832fb16cacc2d75e09b7992131d2d7deb07
aasis59 <aasis452711@gmail.com>
2024-02-15 13:43:55 +0545

    added

--- src/component/choose/Why_best.jsx
+++ src/component/choose/Why_best.jsx
@@ -2,12 +2,12 @@ import { Typography } from "@material-tailwind/react";
 
 function Why_best() {
   return (
-    <section className=" back_ground_one py-2 px-4 md:py-10 md:px-20">
-      <div className="text-center mb-2 md:mb-10">
-        <Typography variant="h2">Why Choose Ashirwad?</Typography>
+    <section className=" back_ground_one pb-20  ">
+      <div className="text-center py-10 ">
+        <Typography variant="h2" > Why Choose Ashirwad?</Typography>
       </div>
-      <div className="grid grid-cols-1 grid-rows-1 md:grid-cols-2 md:grid-rows-2 gap-5  bg-white rounded-lg shadow-lg shadow-cyan-900/100 mt-2 md:mt-10 p-5 md:p-20 ">
-        <div>
+      <div className="grid grid-cols-1 grid-rows-1 md:grid-cols-2 md:grid-rows-2 gap-5 p-20  bg-white rounded-lg shadow-lg  page-width ">
+        <div className="border  border border-gray-200 shadow-md rounded-lg p-5 ">
           <Typography variant="h4" color="blue" textGradient>
           <span className="mr-3">[ ]</span> ACADEMIC EXCELLENCE
           </Typography>
@@ -15,7 +15,7 @@ function Why_best() {
             We have a history of offering profound academic excellence and world class alumni
           </Typography>
         </div>
-        <div>
+        <div className="border  border border-gray-200 shadow-md rounded-lg p-5">
           <Typography variant="h4" color="blue" textGradient>
           <span className="mr-3">[ ]</span> PASSIONATE AND PROFESSIONAL STAFF
           </Typography>
@@ -23,14 +23,14 @@ function Why_best() {
            Our teaching and non teaching comprises of professionals who have passion what they do and love to make a difference
           </Typography>
         </div>
-        <div>
+        <div className="border  border border-gray-200 shadow-md rounded-lg p-5">
           <Typography variant="h4" color="blue" textGradient>
           <span className="mr-3">[ ]</span> SECURITY
           </Typography>
           <Typography className="mt-3" variant="body">
            We partner with top security firms to provides 24/7 security in our establishments. Our facilities are also equiped  with security cameras, providing all round security for all our student          </Typography>
         </div>
-        <div>
+        <div className="border  border border-gray-200 shadow-md rounded-lg p-5">
           <Typography variant="h4" color="blue" textGradient>
           <span className="mr-3">[ ]</span> DIVERSITY
           </Typography>
--- src/component/courses/Courses.jsx
+++ src/component/courses/Courses.jsx
@@ -12,32 +12,33 @@ import plustwo from "../../assets/images/+2.jpg";
 
 function Courses() {
   return (
-    <section className="mb-10">
-      <div className="text-center mt-5 p-8 ">
+    <section className="mb-10 ">
+      <div className="text-center mt-5 p-8  ">
       <Typography variant="h2" >
       Popular Courses
     </Typography>
       </div>
     <div className="grid grid-cols-1 grid-rows-1 gap:2  md:grid-cols-3 md:grid-rows-1 md:gap-5 page-width  ">
-      <div className="">
-        <Card className=" mt-10 md:mt-15 ">
-          <CardHeader color="blue-gray" className="relative">
-            <img src={bbm} alt="card-image" className=" w-full h-auto object-cover" />
+    <div>
+        <Card className=" mt-10 md:mt-15 " >
+          <CardHeader color="blue-gray" className="relative ">
+            <img src={plustwo} alt="card-image" className=" w-full h-auto object-cover" />
           </CardHeader>
           <CardBody>
             <Typography variant="h6" color="blue-gray" className="mb-2">
-             <a href="" className="">Bachelor of Business Management (BBM)</a>
+              <a className="" href="">
+                +2 in Management
+              </a>
             </Typography>
-            <Typography  >
-              Bachelor of Business Management (BBM) is the four year degree
-              program comprising 8 semesters divided over 120 credit hours,
-              conducted by Tribhuvan University (TU). This course is designed in
-              such a way that this can be cornerstone for aspiring business
-              leaders and qualified managers of future.
+            <Typography >
+              +2 management is a two years course. This provides the basic
+              principles and knowledge about the management stream. This course
+              of higher secondary level aims to set a base for students and
+              motivates them to build their career in the management field.
             </Typography>
           </CardBody>
           <CardFooter className="pt-0">
-            <Button className="">Read More</Button>
+            <Button>Read More</Button>
           </CardFooter>
         </Card>
       </div>
@@ -65,29 +66,30 @@ function Courses() {
           </CardFooter>
         </Card>
       </div>
-      <div>
+      <div className="">
         <Card className=" mt-10 md:mt-15 ">
-          <CardHeader color="blue-gray" className="relative ">
-            <img src={plustwo} alt="card-image" className=" w-full h-auto object-cover" />
+          <CardHeader color="blue-gray" className="relative">
+            <img src={bbm} alt="card-image" className=" w-full h-auto object-cover" />
           </CardHeader>
           <CardBody>
             <Typography variant="h6" color="blue-gray" className="mb-2">
-              <a className="" href="">
-                +2 in Management
-              </a>
+             <a href="" className="">Bachelor of Business Management (BBM)</a>
             </Typography>
-            <Typography >
-              +2 management is a two years course. This provides the basic
-              principles and knowledge about the management stream. This course
-              of higher secondary level aims to set a base for students and
-              motivates them to build their career in the management field.
+            <Typography  >
+              Bachelor of Business Management (BBM) is the four year degree
+              program comprising 8 semesters divided over 120 credit hours,
+              conducted by Tribhuvan University (TU). This course is designed in
+              such a way that this can be cornerstone for aspiring business
+              leaders and qualified managers of future.
             </Typography>
           </CardBody>
           <CardFooter className="pt-0">
-            <Button>Read More</Button>
+            <Button className="">Read More</Button>
           </CardFooter>
         </Card>
       </div>
+      
+      
     </div>
     </section>
   );
